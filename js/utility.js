e3b5b6089b6e1b09924ddae46e69ecf1565bfc57
Vishal Abhang <vishalabhang55@yahoo.com>
2021-02-03 19:19:02 +0530

    Update utility.js

--- js/utility.js
+++ js/utility.js
@@ -35,10 +35,10 @@ function downloadURI(data) {
     console.log(data);
     if (navigator.canShare && navigator.canShare(data)) {
         navigator.share(data)
-            .then(() => console.log('Share was successful.'))
-            .catch((error) => console.log('Sharing failed', error));
+            .then(() => alert('Share was successful.'))
+            .catch((error) => alert('Sharing failed', error));
     } else {
-        console.log(`Your system doesn't support sharing files.`);
+        alert(`Your system doesn't support sharing files.`);
     }
     /*
     navigator.share({
